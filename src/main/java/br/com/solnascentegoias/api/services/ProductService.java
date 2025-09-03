84f6473cee41900b73478bd37c231e78ec0d1ef3
bioolegar1 <bioolegari@gmail.com>
2025-09-03 15:44:15 -0300

    feat: Services, escrito completo, adicionando a logica de negocio a API

--- src/main/java/br/com/solnascentegoias/api/services/ProductService.java
+++ src/main/java/br/com/solnascentegoias/api/services/ProductService.java
@@ -3,6 +3,7 @@ package br.com.solnascentegoias.api.services;
 import br.com.solnascentegoias.api.dtos.ProductRequestDTO;
 import br.com.solnascentegoias.api.dtos.ProductResponseDTO;
 import br.com.solnascentegoias.api.entities.Product;
+import br.com.solnascentegoias.api.entities.ProductImage;
 import br.com.solnascentegoias.api.repositories.ProductRepository;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
@@ -16,17 +17,24 @@ import java.util.UUID;
 
 @Service
 public class ProductService {
-
     private final ProductRepository productRepository;
     private final MinioClient minioClient;
+    private final String bucketName;
+    private final String minioEndpoint;
 
-    @Value("${minio.bucket}")
-    private String bucketName;
 
-    public ProductService(ProductRepository productRepository, MinioClient minioClient) {
+    public ProductService(ProductRepository productRepository,
+                          MinioClient minioClient,
+                          @Value("${minio.bucket-name}") String bucketName,
+                          @Value("${minio.bucket.endpoint") String minioEndpoint) {
+
         this.productRepository = productRepository;
         this.minioClient = minioClient;
+        this.bucketName = bucketName;
+        this.minioEndpoint = minioEndpoint;
     }
+
+
 
     @Transactional //tudo funciona ou nada é salvo no banco
     public ProductResponseDTO createProduct(ProductRequestDTO productDTO, List<MultipartFile> files){
@@ -36,7 +44,7 @@ public class ProductService {
         product.setName(productDTO.getName());
         product.setDesciption(productDTO.getDesciption());
         product.setDesciption(productDTO.getDesciption());
-        Product saveProduct = productRepository.save(product);
+        Product savedProduct = productRepository.save(product);
 
         //Upload das Imagens
         int discplayOrder = 1;
@@ -55,10 +63,38 @@ public class ProductService {
                 );
 
                 //PUBLIC URL
-                String imageUrl = String.format("%s/%s/%s", minioClient.getEndpoint(), bucketName, fileName);
+                String imageUrl = String.format("%s/%s/%s", this.minioEndpoint, this.bucketName, fileName);
 
+                //Cria  e salva a entidade  ProductImage
+                ProductImage productImage = new ProductImage();
+                productImage.setImageUrl(imageUrl);
+                productImage.setDisplayOrder(discplayOrder++);
+                savedProduct.addImage(productImage);
 
+            } catch (Exception e) {
+                //caso aja erro, lança uma exceção para o Spring reverter a transação
+                throw new RuntimeException("Erro ao fazer upload do arquivo" + e.getMessage(), e);
             }
+
         }
+        //salva o produto novamente com as img associadas
+        Product finalProduct = productRepository.save(savedProduct);
+        //retorna o DTO de resposta
+        return ProductResponseDTO.fromEntity(finalProduct);
+
     }
+
+    public List<ProductResponseDTO> listAllProducts(){
+        return productRepository.findAll().stream()
+                .map(ProductResponseDTO::fromEntity)
+                .toList();
+    }
+
+    public ProductResponseDTO findProductById(long id) {
+        Product product =  productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado com id: " + id));
+        return ProductResponseDTO.fromEntity(product);
+    }
+
 }
+
